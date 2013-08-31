380ca8bb8ebf6b73b0d782bac4a21d259b4ebb70
Lorant Pinter <lorant.pinter@prezi.com>
2013-08-31 03:55:47 +0200

    Handle inter-project dependencies

--- src/main/groovy/com/prezi/gradle/CompileHaxe.groovy
+++ src/main/groovy/com/prezi/gradle/CompileHaxe.groovy
@@ -2,7 +2,9 @@ package com.prezi.gradle
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ModuleDependency
+import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.artifacts.PublishArtifact
 import org.gradle.api.file.FileCollection
 import org.gradle.api.internal.file.FileResolver
@@ -10,13 +12,20 @@ import org.gradle.api.internal.file.UnionFileCollection
 import org.gradle.api.internal.file.copy.FileCopyActionImpl
 import org.gradle.api.internal.file.copy.FileCopySpecVisitor
 import org.gradle.api.internal.file.copy.SyncCopySpecVisitor
-import org.gradle.api.tasks.*
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.SkipWhenEmpty
+import org.gradle.api.tasks.TaskAction
 import org.gradle.internal.reflect.Instantiator
 import org.gradle.process.internal.ExecException
 
 class CompileHaxe extends DefaultTask {
 
 	public static final String EXTRACTED_HAXELIBS_PATH = "haxelibs"
+
+	PublishArtifact sourceBundle
 
 	@TaskAction
 	void compile()
@@ -27,11 +36,10 @@ class CompileHaxe extends DefaultTask {
 		sourcePath.addAll(sources.files)
 		resourcePath.addAll(resources.files)
 
-		if (configuration != null)
-		{
-			configuration.hierarchy.each { Configuration config ->
-				extractDependenciesFrom(config, sourcePath, resourcePath)
-			}
+		def configuration = getConfiguration()
+
+		configuration.hierarchy.each { Configuration config ->
+			extractDependenciesFrom(config, sourcePath, resourcePath)
 		}
 
 		String cmd = new CmdBuilder()
@@ -56,56 +64,76 @@ class CompileHaxe extends DefaultTask {
 			throw new ExecException("Command finished with non-zero exit value (${res.exitValue}):\n${cmd}")
 		}
 
-		if (archive)
+		FileResolver fileResolver = getServices().get(FileResolver.class)
+		Instantiator instantiator = getServices().get(Instantiator.class)
+		def copyAction = new HarCopyAction(instantiator, fileResolver, temporaryDirFactory,
+				getSourceArchive(), sources, resources)
+		copyAction.execute()
+	}
+
+	public PublishArtifact getSourceBundle()
+	{
+		if (sourceBundle == null)
 		{
-			FileResolver fileResolver = getServices().get(FileResolver.class)
-			Instantiator instantiator = getServices().get(Instantiator.class)
-			def copyAction = new HarCopyAction(instantiator, fileResolver, temporaryDirFactory,
-					getSourceArchive(), sources, resources)
-			copyAction.execute()
+			sourceBundle = new HarPublishArtifact(this, getSourceArchive())
 		}
+		return sourceBundle
 	}
 
 	void extractDependenciesFrom(Configuration configuration, Set<File> sourcePath, Set<File> resourcePath)
 	{
 		configuration.dependencies.each { ModuleDependency dependency ->
-			configuration.files(dependency).each { File file ->
-				Instantiator instantiator = getServices().get(Instantiator.class);
-				FileResolver fileResolver = getServices().get(FileResolver.class);
+			if (dependency instanceof ProjectDependency)
+			{
+				def projectDependency = dependency as ProjectDependency
+				def dependentConfiguration = projectDependency.projectConfiguration
+				extractDependenciesFrom(dependentConfiguration, sourcePath, resourcePath)
 
-				def targetPath = project.file("${project.buildDir}/${EXTRACTED_HAXELIBS_PATH}/${dependency.name}")
-
-				def copy = new FileCopyActionImpl(instantiator, fileResolver, new SyncCopySpecVisitor(new FileCopySpecVisitor()));
-				copy.from(project.zipTree(file))
-				copy.into targetPath
-				copy.execute()
-
-				if (dependency.group == "haxelib")
-				{
-					sourcePath.add(targetPath)
+				dependentConfiguration.allArtifacts.withType(HarPublishArtifact) { HarPublishArtifact artifact ->
+					extractFile(
+							artifact.name + (artifact.classifier == null ? "" : "-" + artifact.classifier),
+							artifact.file,
+							false,
+							sourcePath,
+							resourcePath)
 				}
-				else
-				{
-					def sources = new File(targetPath, "sources")
-					def resources = new File(targetPath, "resources")
-					if (sources.exists()) sourcePath.add(sources)
-					if (resources.exists()) sourcePath.add(resources)
+			}
+			else
+			{
+				configuration.files(dependency).each { File file ->
+					extractFile(dependency.name, file, dependency.group == "haxelib", sourcePath, resourcePath)
 				}
 			}
 		}
 	}
 
-	public PublishArtifact getSourceBundle()
+	private void extractFile(String name, File file, boolean legacyHaxelib, Set<File> sourcePath, Set<File> resourcePath)
 	{
-		return new HarPublishArtifact(this, getSourceArchive())
+		def targetPath = project.file("${project.buildDir}/${EXTRACTED_HAXELIBS_PATH}/${name}")
+		println "Extracting Haxe library file: " + file
+		Instantiator instantiator = getServices().get(Instantiator.class);
+		FileResolver fileResolver = getServices().get(FileResolver.class);
+
+		def copy = new FileCopyActionImpl(instantiator, fileResolver, new SyncCopySpecVisitor(new FileCopySpecVisitor()));
+		copy.from(project.zipTree(file))
+		copy.into targetPath
+		copy.execute()
+
+		// TODO Determine this based on the manifest
+		if (legacyHaxelib)
+		{
+			sourcePath.add(targetPath)
+		} else
+		{
+			def sources = new File(targetPath, "sources")
+			def resources = new File(targetPath, "resources")
+			if (sources.exists()) sourcePath.add(sources)
+			if (resources.exists()) sourcePath.add(resources)
+		}
 	}
 
 	public File getSourceArchive()
 	{
-		if (!archive)
-		{
-			return null
-		}
 		return new File(project.buildDir, getFullName() + ".har")
 	}
 
@@ -164,11 +192,18 @@ class CompileHaxe extends DefaultTask {
 
 	boolean debug
 
-	boolean archive = true
-
 	String classifier
 
 	String baseName
+
+	public Configuration getConfiguration()
+	{
+		if (configuration == null)
+		{
+			return project.configurations[Dependency.DEFAULT_CONFIGURATION]
+		}
+		return configuration
+	}
 
 	public void configuration(Configuration configuration)
 	{
@@ -279,11 +314,6 @@ class CompileHaxe extends DefaultTask {
 	public void flag(String flag)
 	{
 		flags += " $flag"
-	}
-
-	public archive(boolean archive)
-	{
-		this.archive = archive
 	}
 
 	public baseName(String baseName)
--- src/main/groovy/com/prezi/gradle/HaxePlugin.groovy
+++ src/main/groovy/com/prezi/gradle/HaxePlugin.groovy
@@ -2,6 +2,8 @@ package com.prezi.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.internal.tasks.TaskResolver
 import org.gradle.api.tasks.Delete
 import org.gradle.configuration.project.ProjectConfigurationActionContainer
@@ -32,12 +34,37 @@ class HaxePlugin implements Plugin<Project> {
 		def cleanTask = project.tasks.create("clean", Delete)
 		cleanTask.delete(project.buildDir)
 
-		def buildTask = project.tasks.create("build")
-		buildTask.group = BUILD_TASKS_GROUP
-		buildTask.description = "Build all Haxe artifacts"
-		project.tasks.withType(CompileHaxe) { CompileHaxe compileTask ->
-			compileTask.group = BUILD_TASKS_GROUP
-			buildTask.dependsOn compileTask
+		// Create default configuration
+		if (project.configurations.findByName(Dependency.DEFAULT_CONFIGURATION) == null)
+		{
+			project.configurations.create(Dependency.DEFAULT_CONFIGURATION)
+		}
+
+		// Add build task
+		def buildTask = project.tasks.findByName("build")
+		if (buildTask == null)
+		{
+			buildTask = project.tasks.create("build")
+			buildTask.group = BUILD_TASKS_GROUP
+			buildTask.description = "Build all Haxe artifacts"
+		}
+		project.beforeEvaluate {
+			project.tasks.withType(CompileHaxe) { CompileHaxe compileTask ->
+				compileTask.group = BUILD_TASKS_GROUP
+				buildTask.dependsOn compileTask
+			}
+		}
+
+		project.afterEvaluate {
+			project.tasks.withType(CompileHaxe) { CompileHaxe compileTask ->
+				compileTask.configuration.artifacts.add(compileTask.sourceBundle)
+
+				compileTask.configuration.dependencies.withType(ProjectDependency) { ProjectDependency dependency ->
+					dependency.projectConfiguration.allArtifacts.withType(HarPublishArtifact) { HarPublishArtifact artifact ->
+						compileTask.dependsOn(artifact.buildDependencies.getDependencies(compileTask))
+					}
+				}
+			}
 		}
 	}
 }
