69be76ef1697a7be524f5649df481a2350287bae
Lorant Pinter <lorant.pinter@prezi.com>
2013-09-09 15:44:37 +0200

    Show warning when using deprecated compiler class

--- src/main/groovy/com/prezi/gradle/task/CompileHaxe.groovy
+++ src/main/groovy/com/prezi/gradle/task/CompileHaxe.groovy
@@ -1,7 +1,14 @@
 package com.prezi.gradle.task
+
+import org.gradle.api.tasks.TaskAction;
+
 /**
  * Only here for backwards compatibility, use {@link com.prezi.gradle.haxe.CompileHaxe} instead.
  */
 @Deprecated
 class CompileHaxe extends com.prezi.gradle.haxe.CompileHaxe {
+	CompileHaxe()
+	{
+		logger.warn("Please don\'t use '${CompileHaxe}' as it is deprecated. Use '${com.prezi.gradle.haxe.CompileHaxe}' instead.")
+	}
 }
