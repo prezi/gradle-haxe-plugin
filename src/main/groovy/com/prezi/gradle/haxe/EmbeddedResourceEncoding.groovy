5a3eb2b9fb366cd448ade2fa2be3783f34abdbf0
Lorant Pinter <lorant.pinter@prezi.com>
2013-10-02 20:21:33 +0200

    Put resource names last in MANIFEST.MF and forbid '@' in file names as well

--- src/main/groovy/com/prezi/gradle/haxe/EmbeddedResourceEncoding.groovy
+++ src/main/groovy/com/prezi/gradle/haxe/EmbeddedResourceEncoding.groovy
@@ -9,15 +9,19 @@ class EmbeddedResourceEncoding {
 			{
 				throw new IllegalArgumentException("Resource name must not contain '@': " + name)
 			}
+			if (resource.name.contains('@'))
+			{
+				throw new IllegalArgumentException("Resource file name must not contain '@': " + name)
+			}
 			if (encoded.length() > 0)
 			{
 				encoded.append ' '
 			}
-			encoded.append URLEncoder.encode(name, "utf-8")
+			encoded.append URLEncoder.encode(resource.name, "utf-8")
 			if (name != resource.name)
 			{
 				encoded.append '@'
-				encoded.append URLEncoder.encode(resource.name, "utf-8")
+				encoded.append URLEncoder.encode(name, "utf-8")
 			}
 		}
 		return encoded.toString()
@@ -29,9 +33,9 @@ class EmbeddedResourceEncoding {
 		if (encoded != null && !encoded.empty)
 		{
 			encoded.split(' ').each { String res ->
-				def (nameEnc, fileNameEnc) = res.split('@', 2)
-				def name = URLDecoder.decode(nameEnc, "utf-8")
-				def fileName = fileNameEnc ? URLDecoder.decode(fileNameEnc, "utf-8") : name
+				def (fileNameEnc, nameEnc) = res.split('@', 2)
+				def fileName = URLDecoder.decode(fileNameEnc, "utf-8")
+				def name = nameEnc ? URLDecoder.decode(nameEnc, "utf-8") : fileName
 				resources.put name, new File(baseDir, fileName)
 			}
 		}
