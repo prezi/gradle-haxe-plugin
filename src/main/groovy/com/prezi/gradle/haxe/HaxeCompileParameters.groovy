438adea4317210eaceafea437c5fd88ba960fd2a
Lorant Pinter <lorant.pinter@prezi.com>
2014-03-26 14:51:39 +0100

    Mark input properties with @Input

--- src/main/groovy/com/prezi/gradle/haxe/HaxeCompileParameters.groovy
+++ src/main/groovy/com/prezi/gradle/haxe/HaxeCompileParameters.groovy
@@ -1,43 +1,51 @@
 package com.prezi.gradle.haxe
 
 import org.gradle.api.internal.IConventionAware
+import org.gradle.api.tasks.Input
 
 class HaxeCompileParameters {
 
+	@Input
 	String main
 	public main(String main) {
 		this.main = main
 	}
 
+	@Input
 	List<String> macros = []
 	public macro(String m)
 	{
 		macros.add(m)
 	}
 
+	@Input
 	LinkedHashSet<String> includes = []
 	public include(String thing)
 	{
 		includes.add(thing)
 	}
 
+	@Input
 	LinkedHashSet<String> excludes = []
 	public exclude(String thing)
 	{
 		excludes.add(thing)
 	}
 
+	@Input
 	LinkedHashSet<String> flagList = []
 	public void flag(String... flag)
 	{
 		flagList.addAll(flag)
 	}
 
+	@Input
 	boolean debug
 	public debug(boolean debug) {
 		this.debug = debug
 	}
 
+	@Input
 	String spaghetti
 	public spaghetti(String output) {
 		if (!(output in ["module", "application"])) {
