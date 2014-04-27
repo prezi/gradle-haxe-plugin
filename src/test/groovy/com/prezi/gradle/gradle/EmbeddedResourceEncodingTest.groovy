93454708ce787dee9e609b8ab10031441e87a79a
Lorant Pinter <lorant.pinter@prezi.com>
2014-04-27 18:22:53 +0200

    Converted tests to use Spock

--- src/test/groovy/com/prezi/gradle/gradle/EmbeddedResourceEncodingTest.groovy
+++ src/test/groovy/com/prezi/gradle/gradle/EmbeddedResourceEncodingTest.groovy
@@ -1,113 +1,121 @@
 package com.prezi.gradle.gradle
 
 import com.prezi.haxe.gradle.EmbeddedResourceEncoding
-import org.junit.Test
+import spock.lang.Specification
 
-import static org.hamcrest.CoreMatchers.hasItem
-import static org.hamcrest.CoreMatchers.hasItems
-import static org.junit.Assert.assertThat
+class EmbeddedResourceEncodingTest extends Specification {
 
-class EmbeddedResourceEncodingTest {
-
-	@Test
-	public void testEncodeEmpty()
-	{
+	def "encode empty"() {
 		def encoded = EmbeddedResourceEncoding.encode([:])
-		assertThat encoded, is("")
+
+		expect:
+		encoded == ""
 	}
 
-	@Test
-	public void testDecodeEmpty()
-	{
-		assertThat EmbeddedResourceEncoding.decode(null, new File(".")).size(), is(0)
-		assertThat EmbeddedResourceEncoding.decode("", new File(".")).size(), is(0)
+	def "decode null"() {
+		def decoded = EmbeddedResourceEncoding.decode(null, new File("."))
+
+		expect:
+		decoded.isEmpty()
 	}
 
-	@Test
-	public void testEncodeSingle()
-	{
+	def "decode empty"() {
+		def decoded = EmbeddedResourceEncoding.decode("", new File("."))
+
+		expect:
+		decoded.isEmpty()
+	}
+
+	def "encode single"() {
 		def encoded = EmbeddedResourceEncoding.encode([
 			tibor: new File("/some/folder/somewhere/tibor.gif")
 		])
-		assertThat encoded, is("tibor.gif@tibor")
+
+		expect:
+		encoded == "tibor.gif@tibor"
 	}
 
-	@Test
-	public void testDecodeSingle()
-	{
+	def "decode single"() {
 		def decoded = EmbeddedResourceEncoding.decode("tibor.gif@tibor", new File("."))
-		assertThat decoded.size(), is(1)
-		assertThat decoded.keySet(), hasItem("tibor")
-		assertThat decoded["tibor"], is(new File("./tibor.gif"))
+
+		expect:
+		decoded.size() == 1
+		decoded.get("tibor") == new File("./tibor.gif")
 	}
 
-	@Test
-	public void testEncodeSingleWithMatchingName()
-	{
+	def "encode single with matching name"() {
 		def encoded = EmbeddedResourceEncoding.encode([
 			"tibor.gif": new File("/some/folder/somewhere/tibor.gif")
 		])
-		assertThat encoded, is("tibor.gif")
+
+		expect:
+		encoded == "tibor.gif"
 	}
 
-	@Test
-	public void testDecodeSingleWithMatchingName()
-	{
+	def "decode single with matching name"() {
 		def decoded = EmbeddedResourceEncoding.decode("tibor.gif@tibor.gif", new File("."))
-		assertThat decoded.size(), is(1)
-		assertThat decoded.keySet(), hasItem("tibor.gif")
-		assertThat decoded["tibor.gif"], is(new File("./tibor.gif"))
+
+		expect:
+		decoded.size() == 1
+		decoded.get("tibor.gif") == new File("./tibor.gif")
 	}
 
-	@Test
-	public void testEncodeMultiple()
-	{
+	def "encode multiple"() {
 		def encoded = EmbeddedResourceEncoding.encode([
 			tibor: new File("/some/folder/somewhere/tibor.gif"),
 			geza: new File("/another/folder/somewhere/geza.gif"),
 		])
-		assertThat encoded, is("tibor.gif@tibor geza.gif@geza")
+
+		expect:
+		encoded == "tibor.gif@tibor geza.gif@geza"
 	}
 
-	@Test
-	public void testDecodeMultiple()
-	{
+	def "decode multiple"() {
 		def decoded = EmbeddedResourceEncoding.decode("tibor.gif@tibor geza.gif@geza", new File("."))
-		assertThat decoded.size(), is(2)
-		assertThat decoded.keySet(), hasItems("tibor", "geza")
-		assertThat decoded["tibor"], is(new File("./tibor.gif"))
-		assertThat decoded["geza"], is(new File("./geza.gif"))
+
+		expect:
+		decoded.size() == 2
+		decoded.get("tibor") == new File("./tibor.gif")
+		decoded.get("geza") == new File("./geza.gif")
 	}
 
-	@Test
-	public void testEncodeAndDecodeTricky()
-	{
+	def "encode tricky"() {
 		def encoded = EmbeddedResourceEncoding.encode([
-			"t ibor": new File("tibor.gif"),
-			"lajos": new File("l ajos.gif")
+				"t ibor": new File("tibor.gif"),
+				"lajos" : new File("l ajos.gif")
 		])
-		assertThat encoded, is("tibor.gif@t+ibor l+ajos.gif@lajos")
 
-		def decoded = EmbeddedResourceEncoding.decode(encoded, new File("."))
-		assertThat decoded.size(), is(2)
-		assertThat decoded.keySet(), hasItems("t ibor", "lajos")
-		assertThat decoded["t ibor"], is(new File("./tibor.gif"))
-		assertThat decoded["lajos"], is(new File("./l ajos.gif"))
+		expect:
+		encoded == "tibor.gif@t+ibor l+ajos.gif@lajos"
 	}
 
-	@Test(expected = IllegalArgumentException)
-	public void testEncodeWithAtSignInName()
-	{
+	def "decode tricky"() {
+		def encoded = "tibor.gif@t+ibor l+ajos.gif@lajos"
+		def decoded = EmbeddedResourceEncoding.decode(encoded, new File("."))
+
+		expect:
+		decoded.size() == 2
+		decoded["t ibor"] == new File("./tibor.gif")
+		decoded["lajos"] == new File("./l ajos.gif")
+	}
+
+	def "encode with '@' in name"() {
+		when:
 		EmbeddedResourceEncoding.encode([
 			"tibor@geza": new File("tibor-geza.gif")
 		])
+
+		then:
+		thrown(IllegalArgumentException)
 	}
 
-	@Test(expected = IllegalArgumentException)
-	public void testEncodeWithAtSignInFileName()
-	{
+	def "encode with '@' in file name"() {
+		when:
 		EmbeddedResourceEncoding.encode([
 			"tibor-geza": new File("tibor@geza.gif")
 		])
+
+		then:
+		thrown(IllegalArgumentException)
 	}
 }
