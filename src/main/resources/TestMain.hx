4032a6a36b49b0da285d97b56e468569cfd0d0a5
david.nemeth.csoka <david.nemeth.csoka@prezi.com>
2015-06-03 15:10:26 +0200

    testmain fixes

--- src/main/resources/TestMain.hx
+++ src/main/resources/TestMain.hx
@@ -1,35 +1,48 @@
 import massive.munit.client.RichPrintClient;
-import massive.munit.client.HTTPClient;
 import massive.munit.client.JUnitReportClient;
-import massive.munit.client.SummaryReportClient;
 import massive.munit.TestRunner;
 
 class TestMain
 {
 	static function main(){	new TestMain(); }
 
+	var junitReportClient = new JUnitReportClient();
 	public function new()
 	{
-		var suites = new Array<Class<massive.munit.TestSuite>>();
-		suites.push(TestSuite);
+		patchSetIntervalForMunit();
 
-		var client = new RichPrintClient();
-
-		var runner:TestRunner = new TestRunner(client);
-//		runner.addResultClient(new HTTPClient(new JUnitReportClient()));
-
+		var runner:TestRunner = new TestRunner(new RichPrintClient());
+		runner.addResultClient(junitReportClient);
 		runner.completionHandler = completionHandler;
-		runner.run(suites);
+		runner.run([TestSuite]);
 	}
 
 	function completionHandler(successful:Bool):Void
 	{
-		try
-		{
-			trace('hello');
-		}
-		catch (e:Dynamic)
-		{
-		}
+		untyped testComplete(junitReportClient.xml.b, successful);
 	}
+
+	function patchSetIntervalForMunit(){
+
+		//munit is passing a string as the first parameter of window.setInterval, which is not supported by nodejs
+		untyped __js__('window.setInterval= (function(){
+
+			var oldSetInterval = window.setInterval;
+
+			return function(){
+				if (typeof arguments[0] == typeof "")
+				{
+					var arg0 = arguments[0];
+					arguments[0] = function (){
+						eval(arg0);
+					};
+				}
+				return oldSetInterval.apply(this, arguments);
+			}
+
+		})();
+		');
+
+	}
+
 }
